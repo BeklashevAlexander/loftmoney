59d5978ecf5b6a2ad0d4e791b774e7cccafb5c40
bekla <beklashev@yandex.ru>
2020-10-17 22:19:43 +0300

    hello world

---
+++ app/src/main/java/com/example/loftmoney/MainActivityHelloWorld.java
@@ -0,0 +1,9 @@
+package com.example.loftmoney;
+
+
+public class MainActivityHelloWorld{
+    public static void MainActivityHelloWorld(String [] args){
+        System.out.println("Hello, world!");
+    }
+}
+
