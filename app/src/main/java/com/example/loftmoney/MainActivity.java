a8ee855c3b63598ce832b841028e416dbe24dbbd
bekla <beklashev@yandex.ru>
2020-10-20 20:47:10 +0300

    MainActivity

--- app/src/main/java/com/example/loftmoney/MainActivity.java
+++ app/src/main/java/com/example/loftmoney/MainActivity.java
@@ -1,7 +1,15 @@
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
+
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView ItemsView;
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item);
 
