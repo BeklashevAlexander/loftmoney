0b496e86a1a30a7f5ad670e1f2ecacd48ea5a266
bekla <beklashev@yandex.ru>
2020-10-20 20:11:01 +0300

    MainActivity

--- app/src/main/java/com/example/loftmoney/MainActivity.java
+++ app/src/main/java/com/example/loftmoney/MainActivity.java
@@ -1,28 +1,4 @@
-package com.agladkov.loftmoney;
+package com.example.loftmoney;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        findViewById(R.id.btnClick).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent newActivity = new Intent(getApplicationContext(), MainActivity.class);
-                newActivity.putExtra("COST_ID", 1);
-                startActivity(newActivity);
-            }
-        });
-
-        // TODO: - Here we will work with recyclerview
-    }
+public class MainActivity {
 }
